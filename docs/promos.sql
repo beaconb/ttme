6ec99bbf29fec15296225b863622c697dac8e568
beaconb <beaconb.mail@gmail.com>
2016-07-08 14:17:42 +0200

    data

---
+++ docs/promos.sql
@@ -0,0 +1,84 @@
+/* 1 */
+{
+    "_id" : ObjectId("575ea27df4de338c2486a3d2"),
+    "description" : "Sólo un uso del cupón",
+    "title" : "Descuento Ebay",
+    "summary" : "Descuento irrepetible en Ebay, para tí",
+    "publishedDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "fromDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "toDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "link" : "http://rover.ebay.com/rover/4/0/8?eecl=1&eesi=ES&i=1b2eba90f3fII28807II7884a6II1b2eba90cebII2&eepc=&bu=44558875694&segname=B14040D-00&crd=20160612132835&emParams=i&sojTags=emid%3Dbu%2Csegname%3Dsegname%2Ccrd%3Dcrd",
+    "urlPhoto" : "http://g01.a.alicdn.com/kf/HTB1MqYDJVXXXXbLXXXXq6xXFXXXU/New-Arrival-Personalized-Usa-Musical-Flag-Throw-Pillowcases-16x16-18x18-font-b-20x20-b-font-24x24.jpg",
+    "urlPhotoMax" : "http://g01.a.alicdn.com/kf/HTB1MqYDJVXXXXbLXXXXq6xXFXXXU/New-Arrival-Personalized-Usa-Musical-Flag-Throw-Pillowcases-16x16-18x18-font-b-20x20-b-font-24x24.jpg",
+    "categoryID" : ObjectId("575ea27df4de338c2486a3d2"),
+    "providerID" : ObjectId("575ea27df4de338c2486a3d2"),
+    "__v" : 0
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("5771180894d57b5811249836"),
+    "description" : "description",
+    "title" : "title",
+    "summary" : "summary",
+    "publishedDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "fromDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "toDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "link" : "link",
+    "urlPhoto" : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCcrClRrqrxAKtfCe4U-sW_6MKuUL7AVnPjwWK0dejNkba6fg9",
+    "urlPhotoMax" : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCcrClRrqrxAKtfCe4U-sW_6MKuUL7AVnPjwWK0dejNkba6fg9",
+    "categoryID" : ObjectId("575ea27df4de338c2486a3d2"),
+    "providerID" : ObjectId("575ea27df4de338c2486a3d2"),
+    "__v" : 0
+}
+
+/* 3 */
+{
+    "_id" : ObjectId("577bcf18cb1d3a75cb9d85e3"),
+    "description" : "description",
+    "title" : "title",
+    "summary" : "summary",
+    "publishedDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "fromDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "toDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "link" : "link",
+    "urlPhoto" : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCcrClRrqrxAKtfCe4U-sW_6MKuUL7AVnPjwWK0dejNkba6fg9",
+    "urlPhotoMax" : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCcrClRrqrxAKtfCe4U-sW_6MKuUL7AVnPjwWK0dejNkba6fg9",
+    "categoryID" : ObjectId("575ea27df4de338c2486a3d2"),
+    "providerID" : ObjectId("575ea27df4de338c2486a3d2"),
+    "__v" : 0
+}
+
+/* 4 */
+{
+    "_id" : ObjectId("577bcf26cb1d3a75cb9d85e4"),
+    "description" : "description",
+    "title" : "title",
+    "summary" : "summary",
+    "publishedDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "fromDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "toDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "link" : "link",
+    "urlPhoto" : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCcrClRrqrxAKtfCe4U-sW_6MKuUL7AVnPjwWK0dejNkba6fg9",
+    "urlPhotoMax" : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCcrClRrqrxAKtfCe4U-sW_6MKuUL7AVnPjwWK0dejNkba6fg9",
+    "categoryID" : ObjectId("575ea27df4de338c2486a3d2"),
+    "providerID" : ObjectId("575ea27df4de338c2486a3d2"),
+    "__v" : 0
+}
+
+/* 5 */
+{
+    "_id" : ObjectId("577bcf37cb1d3a75cb9d85e5"),
+    "description" : "description",
+    "title" : "title",
+    "summary" : "summary",
+    "publishedDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "fromDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "toDate" : ISODate("2016-07-11T22:00:00.000Z"),
+    "link" : "link",
+    "urlPhoto" : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCcrClRrqrxAKtfCe4U-sW_6MKuUL7AVnPjwWK0dejNkba6fg9",
+    "urlPhotoMax" : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQCcrClRrqrxAKtfCe4U-sW_6MKuUL7AVnPjwWK0dejNkba6fg9",
+    "categoryID" : ObjectId("575ea27df4de338c2486a3d2"),
+    "providerID" : ObjectId("575ea27df4de338c2486a3d2"),
+    "__v" : 0
+}
