d603d6efc406ea72416692df92594d231a20ab6e
beaconb <beaconb.mail@gmail.com>
2016-07-14 17:38:16 +0200

    new iteration

--- config/promo.js
+++ config/promo.js
@@ -1,13 +1,46 @@
 var mongoose = require('mongoose'); 
 var promo = require('../config/promoModel');   
+var http = require('http');
+var parseString = require('xml2js').parseString;
+
 mongoose.set('debug', true);
+
 exports.listPromos = function(callback){
-	promo.find({}, function(err, docs) {
+
+//The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
+var options = {
+  host: 'open.api.ebay.com',
+  path: '/shopping?callname=FindPopularItems&responseencoding=XML&appid=RaulGome-trastoo-PRD-b99eca255-d0ae6793&siteid=0&version=713&QueryKeywords=dog'+''
+};
+
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    console.log(str);
+    parseString(str, function (err, result) {
+    console.dir(JSON.stringify(result));
+	});
+  });
+}
+
+http.request(options, callback).end();
+
+/*
+promo.find({}, function(err, docs) {
     if (!err){ 
 		callback({docs});
     } else {throw err;}
 });
+*/
 }
+
 exports.addPromo = function(description,title,summary,publishedDate,fromDate,toDate,link,urlPhoto,urlPhotoMax,categories,providerID,callback){
 	var newpromo = new promo({
 	description : description,
