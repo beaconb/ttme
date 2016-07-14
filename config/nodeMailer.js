7a6594268979818473d67f64da5d58c5d4a123bf
beaconb <beaconb.mail@gmail.com>
2016-07-14 12:21:11 +0200

    new iteration

--- config/nodeMailer.js
+++ config/nodeMailer.js
@@ -1,6 +1,7 @@
 var nodemailer = require('nodemailer'); // email sender function 
+
 exports.sendEmail = function(name, surename, email, subject, query, callback){
-	console.log('VOy a enviar el correo');
+	
     var transporter = nodemailer.createTransport("SMTP",{
       host: 'smtp.trastoo.me',
       port: 465,
@@ -10,19 +11,22 @@ exports.sendEmail = function(name, surename, email, subject, query, callback){
            pass: 'Raul#G0mez'
        }
 	});
+    console.log('transporter creado');
 	var mailOptions = {
        from: 'info@trastoo.me',
        to: email,
        subject: subject,
        text: query
 	};
+  console.log('opciones creado');
 	transporter.sendMail(mailOptions, function(error, callback){
+    console.log('VOy a enviar el correo, con resultado: '+error);
     if (error){
         console.log(error);
         callback({'response':"Se ha producido un error:"+error}); 
     } else {
         console.log("Email sent");
-        callback({'response':"Su consulta se ha procesado correctamente:"+error}); 
+        callback({'response':"Resultado de la operacion OK"}); 
     }
 });
 };
